d89e9c711f211f0808d579afb99610a269cc4278
overf16 <mohammedbenberri@gmail.com>
2023-03-21 16:56:19 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,15 +10,18 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		_putchar(43);
 		return (1);
-	} else if (n == 0)
+	} 
+	else if (n < 0)
 	{
-		putchar(48);
-		return (0);
-	} else if (n < 0)
+		_putchar(45);
+		return (-1);
+	} 
+	else
 	{
 		_putchar('-');
+		return (0)
 	}
-		return (-1);
+	_putchar('\n');
 }
