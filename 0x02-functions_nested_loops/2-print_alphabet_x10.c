d60df4a4481d7db07a5064904d5c649c2ee1788e
overf16 <mohammedbenberri@gmail.com>
2023-03-21 16:20:33 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,9 +9,9 @@ void print_alphabet_x10(void)
 	int i;
 	int j;
 
-	for (j = 1; i <= 122; j++)
+	for (j = 1; i <= 10; j++)
 	{
-		for (j = 97;j <= 122; j++)
+		for (j = 97; j <= 122; j++)
 		{
 			_putchar(j);
 		}
