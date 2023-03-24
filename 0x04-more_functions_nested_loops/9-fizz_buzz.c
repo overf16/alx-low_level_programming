e98d450b380b7aa614bc57fa5a49ef583add48c1
overf16 <mohammedbenberri@gmail.com>
2023-03-23 17:56:41 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,24 +9,26 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i < 100; i++)
-		if (i % 15 == 0)
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
 		{
-			printf("FizzBuzz ");
-		}
-		else if (i % 5 == 0)
+			printf(" Fizz");
+		} else if (i % 5 == 0 && i % 3 != 0)
 		{
-			printf("Buzz ");
-		}
-		else if (i % 3 == 0)
+			printf(" Buzz");
+		} else if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("Fizz ");
-		}
-		else
+			printf(" FizzBuzz");
+		} else if (i == 1)
 		{
-			printf("%d ", i);
+			printf("%d", i);
+		} else
+		{
+			printf(" %d", i);
 		}
-		printf("Buzz");
-		printf("\n");
-		return (0);
-}
+	}
+	printf("\n");
+
+	return (0);
+	}
