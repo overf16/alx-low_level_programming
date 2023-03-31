831edb4e855babf23837de021bdc5afe4f974bcb
overf16 <mohammedbenberri@gmail.com>
2023-03-30 17:34:19 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,21 +8,22 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int f;
+	int i;
 	int j;
 
-	f = 0;
-	while (dest[f] != '\0')
+	i = 0;
+	while (dest[i] != '\0')
 	{
-		f++;
+		i++;
 	}
 	j = 0;
 	while (src[j] != '\0')
 	{
-		dest[f] = src[j];
-		f++;
+		dest[i] = src[j];
+		i++;
 		j++;
 	}
-	dest[f] = '\0';
+
+	dest[i] = '\0';
 	return (dest);
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,4 +1,7 @@
-nt _putchar(char c);
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -11,3 +14,6 @@ char *rot13(char *a);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
+
+#endif
+
