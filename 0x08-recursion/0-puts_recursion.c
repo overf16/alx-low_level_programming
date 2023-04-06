0531f051a06919c64b48d2f8b189ec3d8863b0bf
overf16 <mohammedbenberri@gmail.com>
2023-04-05 17:38:35 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+nclude "main.h"
+/**
+ * _puts_recursion - Function
+ *
+ * Description: prints a string, followed by a new line
+ *
+ * @s: pointer parameter of type char, string to print
+ *
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar( *s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
